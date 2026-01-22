033d81e715f8295c531eff289121bf7b3e0f0855
Lorenzo Soldi <lorenzo.soldi@mizar-tech.com>
2026-01-22 09:50:55 +0100

    fix dimenticanza call metodo

--- backend/src/main/java/riconciliazionetitoli/controller/RiconciliazioneController.java
+++ backend/src/main/java/riconciliazionetitoli/controller/RiconciliazioneController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import riconciliazionetitoli.exception.RiconciliazioneTitoliException;
 import riconciliazionetitoli.request.RiconciliazioneManualeRequest;
 import riconciliazionetitoli.response.ReconciliationResponse;
 import riconciliazionetitoli.service.RiconciliazioneTitoliService;
@@ -27,7 +28,7 @@ public class RiconciliazioneController {
 	
 
 	@PostMapping("/validate")
-	public void validatePosition(@RequestBody RiconciliazioneManualeRequest request) {
-		
+	public void validatePosition(@RequestBody RiconciliazioneManualeRequest request) throws RiconciliazioneTitoliException {
+		riconciliazioneService.updateReconciliation(request);
 	}
 }
